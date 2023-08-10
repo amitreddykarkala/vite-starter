28b3b154b7817ae8f859f343b2d453c53e146bfa
Amit Reddy <amit.reddy@dozee.io>
2023-08-10 15:29:29 +0530

    Auth Context Provider librar0.0.4 integrated.

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,18 @@
-import React from "react";
+import { AuthContextProvider } from "@dozeelabs/dozee-auth-client";
 import { Route, Routes } from "react-router-dom";
 import Dashboard from "./components/Dashboard";
 import DashboardHome from "./components/DashboardHome";
-import Login from "./components/Login";
-import AuthProvider from "./contexts/AuthContext";
 
 function App() {
   return (
     <div className="w-screen h-screen">
-      <AuthProvider>
+      <AuthContextProvider>
         <Routes>
-          <Route path="/login" element={<Login />}></Route>
           <Route path="/" element={<Dashboard />}>
             <Route path="home" element={<DashboardHome />} />
           </Route>
         </Routes>
-      </AuthProvider>
+      </AuthContextProvider>
     </div>
   );
 }
